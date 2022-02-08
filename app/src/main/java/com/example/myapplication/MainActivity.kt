2884a02d7ee770a4de06ade7d5a4eac4a655fdb6
igorpetrov <hstllbg@icloud.com>
2022-02-08 19:39:47 +0500

    Android task 2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val view = findViewById<TextView>(R.id.textView)
+        view.text = "Ваш текст"
+
+        val student = Student(name = "Александр", course = 1)
+        print(student.name)
+        print(student.toString())
+
         setContentView(R.layout.activity_main)
     }
 }
